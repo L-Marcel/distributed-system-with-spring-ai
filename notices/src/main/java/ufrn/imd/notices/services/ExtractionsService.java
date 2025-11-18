331c5898ab64742039cd5de176d7260e68d23e83
L-Marcel <lmgh1312@gmail.com>
2025-11-18 09:47:47 -0300

    [feat] Adicionando rate limit, retry e bulkhead

--- notices/src/main/java/ufrn/imd/notices/services/ExtractionsService.java
+++ notices/src/main/java/ufrn/imd/notices/services/ExtractionsService.java
@@ -7,13 +7,19 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
+import io.github.resilience4j.retry.annotation.Retry;
 import ufrn.imd.commons.models.enums.NoticeStatus;
 
 @FeignClient("extractions")
 public interface ExtractionsService {
   @PostMapping(value = "/{id}", consumes = "application/json")
   @CircuitBreaker(name = "extractions", fallbackMethod = "fallback")
+  @Retry(name = "extractions", fallbackMethod = "fallback")
+  @RateLimiter(name = "extractions", fallbackMethod = "fallback")
+  @Bulkhead(name = "extractions", fallbackMethod = "fallback", type = Bulkhead.Type.THREADPOOL)
   public ResponseEntity<NoticeStatus> request(
     @PathVariable UUID id
   );
